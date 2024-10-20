d0f09607acf07bf7555a5d80dfeb91e6bd03c1f0
wdafert <w.dafert@gmail.com>
2024-10-20 08:27:33 +0100

    Update server to handle all routes

--- server.py
+++ server.py
@@ -9,7 +9,10 @@ def serve_index():
 
 @app.route('/<path:path>')
 def serve_static(path):
-    return send_from_directory(app.static_folder, path)
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
